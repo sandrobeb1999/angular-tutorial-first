85e8ee55f87901b53e7310998ecc6b7f0e7d4577
Sandro <sandrobeb16@gmail.com>
2019-07-08 20:40:07 +0400

    feat(3-forms): Task 1 in progress

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,25 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormControl } from '@angular/forms';
 
+
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.scss']
 })
 export class FormsComponent implements OnInit {
-  items;
   checkedForm;
 
   constructor(private formBuilder: FormBuilder) {
     this.checkedForm = formBuilder.group({
-      nickname: ['', [Validators.minLength(2), this.forbiddenName()]],
-    })
+      nickname: ['', [Validators.minLength(2),  Validators.required, this.forbiddenName()]],
+      email: ['', [Validators.email, Validators.required]],
+      website: ['',[Validators.required]],
+      password: ['' ,[Validators.required],[Validators.pattern('[a-zA-Z0-9]+$')]],
+      cpassword: ['' ,[Validators.required]],
+      number: ['',[Validators.required]],
+      agree: [false,  [Validators.requiredTrue]]
+    },{validator: this.match})
   }
 
   ngOnInit() {
   }
-
-
 
   forbiddenName() {
     return (formControl) => {
@@ -28,10 +32,13 @@ export class FormsComponent implements OnInit {
   }
 
 
+  match(pass) {
+    if (pass.get('password').value !== pass.get('cpassword').value) {
+        return {invalid: true};
+    }
+}
   onSubmit() {
     console.log(this.checkedForm.value);
-
-    this.checkedForm.reset();
   }
 
 
