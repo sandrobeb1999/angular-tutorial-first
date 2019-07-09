ebb22a6f50d30b76deae4e471aa0ce11621a6b1c
Sandro <sandrobeb16@gmail.com>
2019-07-09 19:30:16 +0400

    feat(16-observable-rxjs): Finish tutorial

--- src/app/currency.service.ts
+++ src/app/currency.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { data } from './rates';
-import { from } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,13 +13,13 @@ export class CurrencyService {
 
   constructor() {
     this.result = this.transformObjectToArray(data.rates);
-    this.Observer = from(this.result);
+    this.Observer = new Observable(this.subscribe());
   }
 
   transformObjectToArray(object) {
     const result = [];
     const keys = Object.keys(object);
-    for(const key of keys) {
+    for (const key of keys) {
       const value = object[key];
       const item = {
         currency: key,
@@ -29,7 +29,7 @@ export class CurrencyService {
     }
     return result;
   }
-  
+
 
   filter(cb) {
     this.result = this.result.filter(cb);
@@ -41,19 +41,20 @@ export class CurrencyService {
     return this;
   }
 
-  subscribe(next, complete) {
-    let i = 0;
-
-    for (const item of this.result) {
-      setTimeout(() =>{
-        next(item);
-      }, i*500);
-
-      i++;
-
-    setTimeout(() => {
-    complete();
-    }, i * 500);
-}
-}
-}
+  subscribe() {
+    return (subscriber) => {
+      let i = 0;
+ 
+      for (const item of this.result) {
+ 
+        setTimeout( () => {
+          subscriber.next(item);
+        }, i * 500);
+        i++;
+      }
+      setTimeout( () => {
+        subscriber.complete();
+      }, i * 500);
+    };
+ }
+ }
